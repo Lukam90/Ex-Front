b3087b7e720dea6f9af7aeb84d7f1a201f1f0207
Lukam90 <lucien.hamm@gmail.com>
2023-01-21 16:51:52 +0100

    MAJ Ex Dynamic > HowSum

---
+++ dynamic/fibonacci.js
@@ -0,0 +1,18 @@
+const fib = (n, memo = {}) => {
+    const start = Date.now();
+
+    if (n in memo)  return memo[n];
+    if (n <= 2) return 1;
+
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+
+    const end = Date.now();
+
+    console.log(`T = ${end - start} ms`);
+
+    return memo[n];
+};
+
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
