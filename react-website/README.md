7014fad8c10fc1a3a06ec966e0ab97ae2c1c30f8
Lukam90 <lucien.hamm@gmail.com>
2023-02-14 15:20:56 +0100

    MAJ React Site > Sources

---
+++ react-website/README.md
@@ -0,0 +1,3 @@
+# react-website
+
+[React Website Tutorial - Beginner React JS Project Fully Responsive](https://www.youtube.com/watch?v=I2UBjN5ER4s) de *Brian Design*
---
+++ react-website/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+import Navbar from './components/Navbar';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+	return (
+		<>
+			<Navbar />
+
+			{/*<Router>
+				<Routes>
+					<Route path="/" />
+				</Routes>
+			</Router>*/}
+		</>
+	);
+}
+
+export default App;
