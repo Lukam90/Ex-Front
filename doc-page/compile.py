3665196502653fffc44fb46858e2cc5bd5d60656
Lukam90 <lucien.hamm@gmail.com>
2023-02-28 14:18:37 +0100

    MAJ Ex Doc > Inclusions

--- doc-page/compile.py
+++ doc-page/compile.py
@@ -4,14 +4,45 @@ from jinja2 import Environment, FileSystemLoader
 
 # Lecture
 
-template_env = Environment(loader = FileSystemLoader(searchpath = "."))
-template = template_env.get_template("documentation.jinja")
+sections = []
+
+with open("data/sections.txt", "r") as file:
+    for line in file:
+        title = line.strip()
+        link = title.replace(" ", "_")
+
+        path = title.replace(" ", "-").lower()
+        path = path.replace("...", "-") + ".html"
+
+        sections.append({ "title" : title, "link" : link, "path" : path })
+
+    file.close()
 
 # Ecriture
 
+template_env = Environment(loader = FileSystemLoader(searchpath = "."))
+template = template_env.get_template("documentation.jinja")
+
 with open("documentation.html", "w") as file:
     file.write(
-        template.render()
+        template.render(sections = sections)
     )
 
     file.close()
+
+"""
+    introductions
+what-you-should-already-know
+javascript-and-java
+hello-world
+variables
+declaring-variables
+variable-scope
+global-variables
+constants
+data-types
+if-else-statement
+while-statement
+function-declarations
+reference
+    """
